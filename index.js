0602b22107e166389ff8952b4bce02220b5aec6b
almario12345 <almarioelopre1@gmail.com>
2024-08-25 03:12:22 +0800

    iuiuiu

--- index.js
+++ index.js
@@ -75,36 +75,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, './public')));
 
-// API to fetch PDF file by research ID
-app.get('/pdf/:research_id', (req, res) => {
-    const researchID = req.params.research_id;
 
-    // Retrieve file name from the database based on research ID
-    db.query('SELECT filename FROM researches WHERE research_id = ?', [researchID], (err, result) => {
-        if (err) {
-            console.error('Error retrieving file name:', err);
-            return res.status(500).send('Internal Server Error');
-        }
-        
-        if (result.length > 0) {
-            const fileName = result[0].file_name;
-            const filePath = path.join(__dirname, '../public/pdfs', fileName);
-            
-            // Check if the file exists
-            fs.access(filePath, fs.constants.F_OK, (err) => {
-                if (err) {
-                    console.error('Error accessing file:', err);
-                    return res.status(404).send('File not found');
-                }
-                
-                // Serve the PDF file
-                res.sendFile(filePath);
-            });
-        } else {
-            res.status(404).send('File not found');
-        }
-    });
-});
 
 
 app.get("/", (req, res) => {
