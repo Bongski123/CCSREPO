de590da0a77c4273d747b93468ca3f807ed59f4b
almario12345 <almarioelopre1@gmail.com>
2024-08-28 08:46:46 +0800

    ntntnt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,28 +2,29 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../database/db');
-const {authenticateToken} = require('../authentication/middleware');
+const { authenticateToken } = require('../authentication/middleware');
 const config = require('../authentication/config');
 const secretKey = config.secretKey;
+
 const router = express.Router();
 
-// login
+// Login
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
 
         const getUserQuery = 'SELECT * FROM users WHERE email = ?';
-        const [rows] = await db.promise().execute(getUserQuery, [email]);
+        const [rows] = await db.query(getUserQuery, [email]);
 
         if (rows.length === 0) {
-            return res.status(401).json({ error: 'Invalid ID number or password' });
+            return res.status(401).json({ error: 'Invalid email or password' });
         }
         const user = rows[0];
         
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid ID number or password' });
+            return res.status(401).json({ error: 'Invalid email or password' });
         }
 
         const token = jwt.sign({ userId: user.user_id, email: user.email, name: user.name, roleId: user.role_id }, secretKey, { expiresIn: '1h' });
@@ -34,6 +35,5 @@ router.post('/login', async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-
 
 module.exports = router;
