ba4fe0bb0103307a682f5496b843445c2d2405e6
almario12345 <almarioelopre1@gmail.com>
2024-12-10 02:09:20 +0800

    nhnh

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -157,17 +157,18 @@ router.post('/total/downloads', (req, res) => {
 
 router.get('/top-downloads', (req, res) => {
     const query = `
-      SELECT 
-        r.research_id, 
-        r.title, 
-        r.downloadCount, 
-        GROUP_CONCAT(a.author_name) AS authors
-      FROM researches r
-      LEFT JOIN research_authors ra ON r.research_id = ra.research_id
-      LEFT JOIN authors a ON ra.author_id = a.author_id
-      GROUP BY r.research_id
-      ORDER BY r.downloadCount DESC
-      LIMIT 10;
+     SELECT 
+    r.research_id, 
+    r.title, 
+    COALESCE(r.downloadCount, 0) AS downloadCount, 
+    COALESCE(GROUP_CONCAT(DISTINCT a.author_name ORDER BY a.author_name), 'Unknown') AS authors
+FROM researches r
+LEFT JOIN research_authors ra ON r.research_id = ra.research_id
+LEFT JOIN authors a ON ra.author_id = a.author_id
+GROUP BY r.research_id, r.title, r.downloadCount
+ORDER BY r.downloadCount DESC
+LIMIT 10;
+
     `;
   
     db.query(query, (err, results) => {
