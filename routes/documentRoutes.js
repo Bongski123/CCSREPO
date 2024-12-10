246ef0447ed97b8bbece5ab82ce68ba829e779fa
almario12345 <almarioelopre1@gmail.com>
2024-12-10 21:01:03 +0800

    Ngngng

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -15,7 +15,7 @@ const auth = new google.auth.GoogleAuth({
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
     private_key: process.env.GOOGLE_PRIVATE_KEY,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL || 'ccsrepo@ccsrepository-444308.iam.gserviceaccount.com',
     client_id: process.env.GOOGLE_CLIENT_ID,
     auth_uri: process.env.GOOGLE_AUTH_URI,
     token_uri: process.env.GOOGLE_TOKEN_URI,
