426982711464048819380ce1c1f3ac78967f54b3
almario12345 <almarioelopre1@gmail.com>
2024-12-10 17:37:56 +0800

    Ngng

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 require("dotenv").config(); // For loading environment variables
 
 // Google Drive configuration
-const KEYFILEPATH = process.env.GOOGLE_SERVICE_ACCOUNT_PATH || path.resolve(__dirname, "../apikeys.json");
+const KEYFILEPATH = process.env.GOOGLE_SERVICE_ACCOUNT_PATH;
 const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
 const auth = new google.auth.GoogleAuth({
   keyFile: KEYFILEPATH,
