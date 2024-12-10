cac633421154b59d7a4b47e9fefce76661a4a0f8
almario12345 <almarioelopre1@gmail.com>
2024-12-10 21:26:26 +0800

    ntnt

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -6,24 +6,12 @@ const { google } = require("googleapis");
 const router = express.Router();
 const streamifier = require("streamifier"); // Import streamifier
 require("dotenv").config(); // Load environment variables
-
+const credentials = JSON.parse(fs.readFileSync(path.join(__dirname, '../googleAPI.json')));
 // Google Drive Setup
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 const auth = new google.auth.GoogleAuth({
-  credentials: {
-    type: process.env.GOOGLE_TYPE,
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
-  },
-  scopes: SCOPES, // <-- Use the SCOPES variable here
+  credentials,
+  scopes: ["https://www.googleapis.com/auth/drive.file"]
 });
 
 const drive = google.drive({ version: "v3", auth });
