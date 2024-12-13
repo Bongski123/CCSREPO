4b52d1416675f2520972ca90d0186a29d4a6ab04
almario12345 <almarioelopre1@gmail.com>
2024-12-14 06:10:51 +0800

    nhmhmh

--- routes/FilePrivacy.js
+++ routes/FilePrivacy.js
@@ -93,7 +93,7 @@ router.post('/request-pdf', async (req, res) => {
     // Step 2: Insert the PDF request into the 'pdf_requests' table
     const [insertResult] = await db.query(
       'INSERT INTO pdf_requests (research_id,research_title, requester_name, requester_email, purpose) VALUES (?, ?, ?, ?,?)',
-      [researchTitle, requesterName, requesterEmail, purpose]
+      [researchId,researchTitle, requesterName, requesterEmail, purpose]
     );
 
     if (!insertResult.affectedRows) {
