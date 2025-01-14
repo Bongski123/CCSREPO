367e9a9202dc4d05a66784552d0c8576f9e66b69
almario12345 <almarioelopre1@gmail.com>
2025-01-14 18:46:26 +0800

    Csbf

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -142,7 +142,7 @@ router.post("/upload", upload.single("file"), async (req, res) => {
     }
 
     // Save research metadata to RestDB
-    const restDBUrl = "https://www-ccsnexus-3c3f.restdb.io/rest/researches"; // Replace with your RestDB URL
+    const restDBUrl = "/https/ccsnexus-3c3f.restdb.io/rest/researches"; // Replace with your RestDB URL
     const restDBApiKey = "10de4bbb0fbd2a5ddd74f21ff76bae188fc02"; // Replace with your RestDB API Key
     const researchMetadata = {
       title,
